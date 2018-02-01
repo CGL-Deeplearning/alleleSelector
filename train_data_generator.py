73f08131a27e79cce187deafe4f034ac76da72c5
Kishwar <kishwar.shafin@gmail.com>
2018-01-31 16:46:05 -0800

    try a chromosome 22 run to see if it works :new_moon_with_face:

--- train_data_generator.py
+++ train_data_generator.py
@@ -254,11 +254,11 @@ def genome_level_parallelization(bam_file, ref_file, vcf_file, output_dir, max_t
     :param max_threads: Maximum number of threads to create in chromosome level
     :return: Saves a bed file
     """
-    chr_list = ["chr1", "chr2", "chr3", "chr4", "chr4", "chr5", "chr6", "chr7", "chr8", "chr9", "chr10", "chr11",
-                "chr12", "chr13", "chr14", "chr15", "chr16", "chr17", "chr18", "chr19", "chr20", "chr21", "chr22"]
+    # chr_list = ["chr1", "chr2", "chr3", "chr4", "chr4", "chr5", "chr6", "chr7", "chr8", "chr9", "chr10", "chr11",
+    #             "chr12", "chr13", "chr14", "chr15", "chr16", "chr17", "chr18", "chr19", "chr20", "chr21", "chr22"]
     program_start_time = time.time()
 
-    # chr_list = ["chr3"]
+    chr_list = ["chr22"]
 
     # each chormosome in list
     for chr in chr_list:
