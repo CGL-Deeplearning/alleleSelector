05b3557db0c64a847e2b8d08d103c153c3f58a11
Kishwar <kishwar.shafin@gmail.com>
2018-02-06 10:48:07 -0800

    whole genome ready :boom:

--- train_data_generator.py
+++ train_data_generator.py
@@ -126,7 +126,7 @@ class View:
         start_time = time.time()
         # self.parse_region(start_position=121400000, end_position=121600000)
 
-        self.parse_region(start_position=5454815, end_position=5454820)
+        self.parse_region(start_position=26824100, end_position=26824106)
         end_time = time.time()
         print("TOTAL TIME ELAPSED: ", end_time-start_time)
 
@@ -214,11 +214,11 @@ def genome_level_parallelization(bam_file, ref_file, vcf_file, output_dir, max_t
     :param max_threads: Maximum number of threads to create in chromosome level
     :return: Saves a bed file
     """
-    # chr_list = ["chr1", "chr2", "chr3", "chr4", "chr4", "chr5", "chr6", "chr7", "chr8", "chr9", "chr10", "chr11",
-    #             "chr12", "chr13", "chr14", "chr15", "chr16", "chr17", "chr18", "chr19", "chr20", "chr21", "chr22"]
+    chr_list = ["chr1", "chr2", "chr3", "chr4", "chr4", "chr5", "chr6", "chr7", "chr8", "chr9", "chr10", "chr11",
+                "chr12", "chr13", "chr14", "chr15", "chr16", "chr17", "chr18", "chr19", "chr20", "chr21", "chr22"]
     program_start_time = time.time()
 
-    chr_list = ["chr3"]
+    # chr_list = ["chr3"]
 
     # each chormosome in list
     for chr in chr_list:
